5fc5048d22a4a218a06b0a640f80454d9ac782d2
wangmeng <wangmengc@uniontech.com>
2025-08-14 17:47:57 +0800

    添加gun目录下的util.rs文件

--- gnu/src/lib.rs
+++ gnu/src/lib.rs
@@ -4,17 +4,4 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod util;
